91c2611cb9fa91f98256245c80777d5080e7f117
michael-duke <michael.21duke@gmail.com>
2023-01-03 18:30:22 +0300

    Implement Greeting, Heading and Loading components Strucutre the components into App.js and index.js Allow the store to be consumed by the App.js through the Provider component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import store from './redux/configureStore';
 import App from './App';
 import './index.css';
 
@@ -9,10 +10,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <Router>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </Router>,
 );
-
